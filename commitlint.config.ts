66f67d88752449d0f71277b004a58972b38595fa
Teb Yuma <yumateb@gmail.com>
2025-01-03 12:08:43 +0700

    fix: husky hook add pre-commit

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,10 +1,10 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
+  extends: ['@commitlint/config-conventional'], // Use conventional commit rules
   rules: {
     'type-enum': [
       2, // Error level
       'always', // Always apply this rule
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'chore'], // Only allow these types
+      ['feat', 'init', 'fix', 'docs', 'style', 'refactor', 'chore'], // Only allow these types
     ],
     'subject-case': [2, 'never', ['sentence-case']], // Prevent the subject from being sentence case
   },
